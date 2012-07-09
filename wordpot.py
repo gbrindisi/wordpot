2d65d05f97884026812331f304b412e811b76712
Gianluca Brindisi <g@brindi.si>
2012-07-09 19:29:29 +0200

    Polished the code and the structure. Fixed the logging facility. Added cli options.

--- wordpot.py
+++ wordpot.py
@@ -1,37 +1,50 @@
-from flask import Flask, request, render_template
-from werkzeug.routing import BaseConverter
-import re
+#/usr/bin/env python
+
+try:
+    from flask import Flask, request, render_template
+except ImportError:
+    print "\n[X] Please install Flask:"
+    print "   $ pip install flask\n"
+    exit()
+
+from optparse import OptionParser
+from core.utils import RegexConverter, timthumb
 import logging
 
 app = Flask(__name__)
-
-TIMTHUMB_RE     = re.compile('[tim]*thumb|uploadify', re.I)
-HOST            = '127.0.0.1'
-PORT            = '5000'
-BLOGTITLE       = 'Random Ramblings'
-VERSION         = '2.8'
-
-
-class RegexConverter(BaseConverter):
-    def __init__(self, url_map, *items):
-        super(RegexConverter, self).__init__(url_map)
-        self.regex = items[0]
-
 app.url_map.converters['regex'] = RegexConverter
 
+# -------
+# Globals
+# -------
+
+HOST            = ''
+PORT            = ''
+BLOGTITLE       = ''
+VERSION         = ''
+LOGGER          = logging.getLogger('wordpot')
+
+# -------
+# Logging
+# -------
+
 def logging_setup():
+    # File handler
     fh = logging.FileHandler('wordpot.log')
-    fh.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(message)s')
     fh.setFormatter(formatter)
-    app.logger.addHandler(fh)
 
+    # Add handlers
+    LOGGER.addHandler(fh)
+   
+    # Set level
+    LOGGER.setLevel(logging.INFO)
+    
+    return True
 
-def timthumb(subpath):
-    """ Basic RE check to find timthumb related requests """
-    if TIMTHUMB_RE.search(subpath) is not None:
-        return True
-    return False
+# ------
+# Routes
+# ------
 
 @app.route('/')
 def homepage(file=None, path=None, subpath=None):
@@ -41,34 +54,67 @@ def homepage(file=None, path=None, subpath=None):
 def readme():
     """ Readme probing handler """
     origin = request.remote_addr
-    app.logger.info('%s probed for the readme', origin)
+    LOGGER.info('%s probed for the readme', origin)
     return render_template('readme.html', version=VERSION)
 
 @app.route('/wp-admin<regex("\/.*"):subpath>', methods=['GET', 'POST'])
 def admin(subpath):
     """ Admin panel probing handler """
     origin = request.remote_addr
-    app.logger.info('%s probed for the admin panel with path: %s', origin, subpath)
+    LOGGER.info('%s probed for the admin panel with path: %s', origin, subpath)
     return render_template('wp-login.html', blogtitle=BLOGTITLE, version=VERSION)
 
-@app.route('/wp-content/plugins/<plugin><regex("\/.*"):subpath>', methods=['GET', 'POST'])
-def plugin(plugin, subpath):
+@app.route('/wp-content/plugins/<plugin>', methods=['GET', 'POST'])
+@app.route('/wp-content/plugins/<plugin><regex("(\/.*)"):subpath>', methods=['GET', 'POST'])
+def plugin(plugin, subpath='/'):
     """ Plugin probing handler """
     origin = request.remote_addr
-    app.logger.info('%s probed a plugin: %s', origin, plugin)
+    LOGGER.info('%s probed a plugin: "%s" with path "%s"', origin, plugin, subpath)
     if timthumb(subpath):
-        app.logger.info('%s probed for timthumb: %s', origin, subpath)
+        LOGGER.info('%s probed for timthumb: %s', origin, subpath)
     return render_template('dummy.html', blogtitle=BLOGTITLE, version=VERSION)
 
-@app.route('/wp-content/themes/<theme><regex("\/.*"):subpath>', methods=['GET', 'POST'])
-def theme(theme, subpath):
+@app.route('/wp-content/themes/<plugin>', methods=['GET', 'POST'])
+@app.route('/wp-content/themes/<theme><regex("(\/.*)"):subpath>', methods=['GET', 'POST'])
+def theme(theme, subpath='/'):
     """ Theme probing handler """
     origin = request.remote_addr
-    app.logger.info('%s probed a theme: %s', origin, theme)
+    LOGGER.info('%s probed a theme: "%s" with path "%s"', origin, theme, subpath)
     if timthumb(subpath):
-        app.logger.info('%s probed for timthumb: %s', origin, subpath)
+        LOGGER.info('%s probed for timthumb: %s', origin, subpath)
     return render_template('dummy.html', blogtitle=BLOGTITLE, version=VERSION) 
 
+# -------------
+# Option parser
+# -------------
+
+def parse_options():
+    usage = "usage: %prog [options]"
+
+    parser = OptionParser(usage=usage)
+    parser.add_option('--host', dest='host', default='127.0.0.1', help='Host address')
+    parser.add_option('--port', dest='port', default='80', help='Port number')
+    parser.add_option('--title', dest='title', default='Random Rambling', help='Blog title')
+    parser.add_option('--ver', dest='version', default='2.8', help='Wordpress version')
+
+    (options, args) = parser.parse_args()
+
+    # Ugly!
+    global HOST  
+    global PORT      
+    global BLOGTITLE    
+    global VERSION 
+
+    HOST         = options.host
+    PORT         = options.port
+    BLOGTITLE    = options.title
+    VERSION      = options.version
+
+    return True
+
 if __name__ == '__main__':
-    logging_setup()
-    app.run(debug=True, host=HOST, port=int(PORT))
+    logging_setup() 
+    parse_options()
+
+    LOGGER.info('Honeypot started.')
+    app.run(host=HOST, port=int(PORT))
