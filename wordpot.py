28d384f2140dce7e3690af7e6f3354c39242baa4
Gianluca Brindisi <g@brindi.si>
2012-07-11 21:05:10 +0200

    Adjusted the structure of static files to nearly match a real Wordpress install. Added a redirect to wp-login.php from wp-admin/ Removed useless images/css and improved the template

--- wordpot.py
+++ wordpot.py
@@ -1,7 +1,7 @@
 #/usr/bin/env python
 
 try:
-    from flask import Flask, request, render_template
+    from flask import Flask, request, render_template, make_response, url_for, redirect
 except ImportError:
     print "\n[X] Please install Flask:"
     print "   $ pip install flask\n"
@@ -11,6 +11,7 @@ from optparse import OptionParser
 from core.utils import RegexConverter, timthumb
 import logging
 
+# /*, static_url_path='/wp-content'
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
 
@@ -20,9 +21,13 @@ app.url_map.converters['regex'] = RegexConverter
 
 HOST            = ''
 PORT            = ''
-BLOGTITLE       = ''
-VERSION         = ''
 LOGGER          = logging.getLogger('wordpot')
+TEMPLATE_VARS   = {
+        'BLOGTITLE': '',
+        'VERSION':   '',
+        'THEME':     ''
+                  }
+
 
 # -------
 # Logging
@@ -48,14 +53,14 @@ def logging_setup():
 
 @app.route('/')
 def homepage(file=None, path=None, subpath=None):
-    return render_template('dummy.html', blogtitle=BLOGTITLE, version=VERSION)
+    return render_template('dummy.html', tpl=TEMPLATE_VARS)
 
 @app.route('/readme.html', methods=['GET', 'POST'])
 def readme():
     """ Readme probing handler """
     origin = request.remote_addr
     LOGGER.info('%s probed for the readme', origin)
-    return render_template('readme.html', version=VERSION)
+    return render_template('readme.html', tpl=TEMPLATE_VARS)
 
 @app.route('/xmlrpc.php', methods=['GET', 'POST'])
 def xmlrpc():
@@ -65,12 +70,17 @@ def xmlrpc():
     return render_template('xmlrpc.html') 
 
 @app.route('/wp-login.php', methods=['GET', 'POST'])
+def login():
+    origin = request.remote_addr
+    LOGGER.info('%s probed for the login page', origin)
+    return render_template('wp-login.html', tpl=TEMPLATE_VARS)
+
 @app.route('/wp-admin<regex("\/.*"):subpath>', methods=['GET', 'POST'])
 def admin(subpath='/'):
     """ Admin panel probing handler """
     origin = request.remote_addr
     LOGGER.info('%s probed for the admin panel with path: %s', origin, subpath)
-    return render_template('wp-login.html', blogtitle=BLOGTITLE, version=VERSION)
+    return redirect(url_for('login'))
 
 @app.route('/wp-content/plugins/<plugin>', methods=['GET', 'POST'])
 @app.route('/wp-content/plugins/<plugin><regex("(\/.*)"):subpath>', methods=['GET', 'POST'])
@@ -81,9 +91,9 @@ def plugin(plugin, subpath='/'):
     if timthumb(subpath):
         LOGGER.info('%s probed for timthumb: %s', origin, subpath)
         return render_template('timthumb.html') 
-    return render_template('dummy.html', blogtitle=BLOGTITLE, version=VERSION)
+    return render_template('dummy.html', tpl=TEMPLATE_VARS)
 
-@app.route('/wp-content/themes/<plugin>', methods=['GET', 'POST'])
+@app.route('/wp-content/themes/<theme>', methods=['GET', 'POST'])
 @app.route('/wp-content/themes/<theme><regex("(\/.*)"):subpath>', methods=['GET', 'POST'])
 def theme(theme, subpath='/'):
     """ Theme probing handler """
@@ -91,8 +101,8 @@ def theme(theme, subpath='/'):
     LOGGER.info('%s probed a theme: "%s" with path "%s"', origin, theme, subpath)
     if timthumb(subpath):
         LOGGER.info('%s probed for timthumb: %s', origin, subpath)
-        return render_template('timthumb.html') 
-    return render_template('dummy.html', blogtitle=BLOGTITLE, version=VERSION) 
+        return render_template('timthumb.html')
+    return render_template('dummy.html', tpl=TEMPLATE_VARS) 
 
 # -------------
 # Option parser
@@ -105,6 +115,7 @@ def parse_options():
     parser.add_option('--host', dest='host', default='127.0.0.1', help='Host address')
     parser.add_option('--port', dest='port', default='80', help='Port number')
     parser.add_option('--title', dest='title', default='Random Rambling', help='Blog title')
+    parser.add_option('--theme', dest='theme', default='twentyeleven', help='Default theme name')
     parser.add_option('--ver', dest='version', default='2.8', help='Wordpress version')
 
     (options, args) = parser.parse_args()
@@ -112,13 +123,16 @@ def parse_options():
     # Ugly!
     global HOST  
     global PORT      
-    global BLOGTITLE    
+    global TEMPLATE_VARS
+    global BLOGTITLE   
+    global THEME
     global VERSION 
 
-    HOST         = options.host
-    PORT         = options.port
-    BLOGTITLE    = options.title
-    VERSION      = options.version
+    HOST                        = options.host
+    PORT                        = options.port
+    TEMPLATE_VARS['BLOGTITLE']  = options.title
+    TEMPLATE_VARS['THEME']      = options.theme
+    TEMPLATE_VARS['VERSION']    = options.version
 
     return True
 
